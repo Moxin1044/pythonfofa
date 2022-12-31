9fb6b8b2f8308930bd2bbb25f737585243a5a896
末心 <59173630+Moxin1044@users.noreply.github.com>
2022-12-31 14:05:04 +0800

    Upload
    Upload Pythonfofa

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonfofa',
+    version='1.0.0',
+    packages=['pythonfofa'],
+    url='https://github.com/Moxin1044/pyfofa',
+    license='MIT License',
+    author='Moxin',
+    author_email='1044631097@qq.com',
+    description='FOFA.info Python SDK'
+)
