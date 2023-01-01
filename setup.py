fa881239baa67b6aab3f0f414c67fac8665b5f9f
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 09:20:59 +0800

    update the setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+import io
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pythonfofa',
-    version='1.0.5',
+    version='1.0.7',
     packages=['pythonfofa'],
     url='https://github.com/Moxin1044/pythonfofa',
     license='MIT License',
     author='Moxin',
     author_email='1044631097@qq.com',
-    description='FOFA.info Python SDK'
+    description='FOFA.info Python SDK',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
